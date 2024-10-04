029e3697e4915a58abb28fd824e79ac507466f18
Daniel <danielboone1990@gmail.com>
2024-10-04 18:11:56 +0700

    change settings for github actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,8 +4,6 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
     trace: 'on-first-retry',
